ff365e70fca65d9e5f0567f4eccd23fc728a1298
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-11-05 21:26:34 +0100

    added variables in class Student

--- CLI/Model/Student.cs
+++ CLI/Model/Student.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace CLI.Model
 {
-    internal class Student
+    public class Student
     {
+        enum Status {B, S};
+        public string name { get; set; }
+        public string surname { get; set; }
+
+        public string birthdate { get; set; }
+        public string adress {  get; set; }
+        public int phoneNumber { get; set; }
+        public string email { get; set; }
+        public string indexNm { get; set; }
+        public int stYear { get; set; }
+        public double avGrade { get; set; }
+
+
     }
 }
