6291d33d0305b55e85e278af74e597e159bdd847
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-11-07 11:40:33 +0100

    added new class Professor with its variables

---
+++ CLI/Model/Professor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI.Model
+{
+    public class Professor
+    {
+        public int Id { get; set; } //broj licne karte
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthDate { get; set; }
+        public string Adress { get; set; }
+        public int PhoneNumber { get; set; }   
+        public string Title { get; set; }
+        public int YearS { get; set; } // godine staza
+
+
+
+    }
+}
