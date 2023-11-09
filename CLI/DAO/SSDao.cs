1b87df77a38dd367ae118207b43ce11fab4bd222
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-11-09 12:04:07 +0100

    tried some metods in class SSDao

--- CLI/DAO/SSDao.cs
+++ CLI/DAO/SSDao.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using CLI.Model;
+using CLI.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CLI.DAO
+namespace CLI.DAO;
+
+public class SSDao
 {
-    internal class SSDao
+    /*
+    private readonly Storage<T> _storage;
+
+
+    public VehiclesDAO()
     {
+        _storage = new Storage<Student>("read_me.txt");
+        _students = _storage.Load();
     }
+
+    public List<Student> GetAllVehicles()
+    {
+        return _students;
+    }
+
+    public List<Student> GetAllSubjects()
+    {
+        return _subjects;
+    }
+
+    public List<Student> GetAllProfessors()
+    {
+        return _professors;
+    }
+
+    */
 }
