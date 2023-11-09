9ae633b7f837ff1e0e99298aaf94695c8d186a92
User-Mima <mimasekaric@gmail.com>
2023-11-09 12:01:58 +0100

    added constructors to class Student

--- CLI/Model/Student.cs
+++ CLI/Model/Student.cs
@@ -10,7 +10,7 @@ namespace CLI.Model
     {
         enum Status {B, S};
 
-        public int Id { get; set; }
+        public int Id { get;  set; }
         public string Name { get; set; }
         public string Surname { get; set; }
 
@@ -21,6 +21,25 @@ namespace CLI.Model
         public string IndexNm { get; set; }
         public int StYear { get; set; }
         public double AvGrade { get; set; }
+
+        List<int> Grades = new List<int>();
+        List<string> Subjects = new List<string>();
+
+        public Student ( int id,  string name, string surname, string birthdate, string adress, int phonenumber, string email,string indexnm, int styear, double avgrade  )
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Birthdate = birthdate;
+            Adress = adress;
+            PhoneNumber = phonenumber;
+            Email = email;
+            IndexNm = indexnm;
+            StYear = styear;
+            AvGrade = avgrade;
+
+        }
+
 
 
     }
