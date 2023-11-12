6f159a7bbfa340e82b1aedb873cb6ec4667b6dbd
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-11-12 22:02:04 +0100

    added class ConsoleViewGrade

---
+++ CLI/Console/ConsoleViewGrade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI.Console;
+
+static class ConsoleViewGrade
+{
+    public static int SafeInputGrade()
+    {
+        int input;
+
+        string rawInput = System.Console.ReadLine() ?? string.Empty;
+
+        while (!int.TryParse(rawInput, out input) || (rawInput != "5" && rawInput != "6" && rawInput != "7" && rawInput != "8" && rawInput != "9" && rawInput != "10"))
+        {
+            System.Console.WriteLine("Not a valid number, try again: ");
+
+            rawInput = System.Console.ReadLine() ?? string.Empty;
+        }
+
+        return input;
+    }
+}
