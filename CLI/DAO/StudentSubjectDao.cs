c2a4bccbb8cfbbb3254922cf15471d1ea3c32644
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-11-14 22:05:43 +0100

    trying to add new Dao

---
+++ CLI/DAO/StudentSubjectDao.cs
@@ -0,0 +1,75 @@
+﻿using CLI.Model;
+using CLI.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CLI.DAO;
+
+public class StudentSubjectDao
+{
+    private readonly List<Subject> subjects;
+    private readonly Storage<Subject> _storage;
+
+
+    public SubjectDao()
+    {
+        _storage = new Storage<Subject>("subject.txt");
+        subjects = _storage.Load();
+    }
+
+    private int GenerateId()
+    {
+        if (subjects.Count == 0) return 0;
+        return subjects[^1].Id + 1;
+    }
+
+    public Subject AddSubject(Subject sub)
+    {
+        sub.Id = GenerateId(); //generisi id za svaki predmet
+        subjects.Add(sub);
+        _storage.Save(subjects);
+        return sub;
+    }
+
+    public Subject? UpdateSubject(Subject sub)
+    {
+        Subject? oldsub = GetSubjectById(sub.Id); // sa istim id treba da unesemo nove podatke koji su u sub
+        if (oldsub is null) return null;
+
+        oldsub.Name = sub.Name;
+        oldsub.SYear = sub.SYear;
+        oldsub.NumEspb = sub.NumEspb;
+
+        _storage.Save(subjects);
+        return oldsub;
+    }
+
+    public Subject? RemoveSubject(int id)
+    {
+        Subject? subject = GetSubjectById(id);
+        if (subject == null) return null;
+
+        subjects.Remove(subject);
+        _storage.Save(subjects);
+        return subject;
+    }
+
+    private Subject? GetSubjectById(int id)
+    {
+        return subjects.Find(v => v.Id == id);
+    }
+
+    public List<Subject> GetAllSubjects()
+    {
+        return subjects;
+    }
+
+    public Model.Subject FindSubjectById(List<Subject> subjects, int tId)
+    {
+        return subjects.Find(subjects => subjects.Id == tId);
+    }
+}
