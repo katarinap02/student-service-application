51556a866c855b5f258ecf073df3b2d5753d1e9d
Katarina Petrović <katarina.petrovic02@gmail.com>
2023-12-13 11:16:15 +0100

    added status bar

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GUI
 {
@@ -20,9 +21,20 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateDateTime;
+            timer.Start();
+        }
+
+        private void UpdateDateTime(object sender, EventArgs e)
+        {
+            dateTimeTextBlock.Text = DateTime.Now.ToString("HH:mm yyyy-MM-dd");
         }
     }
 }
